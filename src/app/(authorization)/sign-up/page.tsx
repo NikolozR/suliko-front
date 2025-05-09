6199e054e15b784b2b18ecddb088be4d502a2583
NikolozR <nika.rusishvili.95@gmail.com>
2025-05-09 21:13:40 +0400

    merge done

--- src/app/(authorization)/sign-up/page.tsx
+++ src/app/(authorization)/sign-up/page.tsx
@@ -6,15 +6,15 @@ import SulikoLogoBlack from "../../../../public/Suliko_logo_black.svg";
 
 export default function SignUp() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex items-center w-[50%] justify-center h-screen">
+    <div className="flex flex-col-reverse sm:flex-row justify-center items-center h-screen">
+      <div className="flex relative items-center sm:w-[50%] w-full justify-center sm:h-screen">
         <div>
-          <Image src={SulikoLogoBlack} width={100} className="absolute top-[30px] left-[30px]" alt="Suliko Black Logo" ></Image>
+          <Image src={SulikoLogoBlack} width={100} className="absolute hidden sm:block top-[20px] left-[20px]" alt="Suliko Black Logo" ></Image>
         </div>
         <SulikoForm />
       </div>
-      <div className="w-[50%] h-full suliko-default-bg flex justify-center items-center">
-        <Image src={SulikoLogoWhite} width={400} alt="Suliko White Logo" />
+      <div className="w-full py-[100px] sm:py-0 sm:w-[50%] h-full suliko-default-bg flex justify-center items-center z-10">
+        <Image src={SulikoLogoWhite} width={300} alt="Suliko White Logo" className="lg:w-[400px]"/>
       </div>
     </div>
   );
