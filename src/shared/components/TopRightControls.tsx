b8e9b2be6a8dfedb24bfc2066785f2f2a2ece4e2
Mishkaten <mishkaten@gmail.com>
2025-10-20 14:40:35 +0400

    Vercel fix v3

--- src/shared/components/TopRightControls.tsx
+++ src/shared/components/TopRightControls.tsx
@@ -1,14 +1,6 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
-
 export default function TopRightControls() {
-  const pathname = usePathname();
-
-  // Hide on landing page root paths like "/en" or "/ka" or "/pl" and blog pages
-  const isLanding = /^\/[^\/]+$/.test(pathname || '');
-  const isBlog = /^\/[^\/]+\/blog(\/|$)/.test(pathname || '');
-  
   // Always return null since this component is not being used
   return null;
 }
