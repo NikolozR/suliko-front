9d5b92efd948cb809aea399e1a28f8f7d1f8ef59
jeliaaa <julia.aleqsandre@gmail.com>
2026-06-17 02:47:03 +0400

    error fix

--- src/app/[locale]/payment/callback/page.tsx
+++ src/app/[locale]/payment/callback/page.tsx
@@ -14,7 +14,7 @@ export default function PaymentTestPage() {
         const queryString = searchParams.toString();
         console.log(queryString)
         const res = await fetch(
-          `https://test_env.suliko.ge/api/Payment/callback?${queryString}`,
+          `https://content.api24.ge/api/Payment/callback?${queryString}`,
           { method: 'GET' }
         );
 
